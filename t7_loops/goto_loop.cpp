f02daf5fbc32a2c2f47d9380d584192dc2992839
Steve Manuel <stevenhmanuel@gmail.com>
2013-09-03 01:44:32 -0700

    writing loop with goto statement

---
+++ t7_loops/goto_loop.cpp
@@ -0,0 +1,15 @@
+// creating a loop with goto
+
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+  int n = 10;
+  loop:
+  cout << n << ", ";
+  n--;
+  if (n > 0) goto loop;
+  cout << "FIRE!" << endl;
+  return 0;
+}
