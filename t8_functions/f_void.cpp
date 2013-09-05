5a207d1ce899e30df6da9282bbaf8aecb63a96c7
Steve Manuel <stevenhmanuel@gmail.com>
2013-09-04 18:21:28 -0700

    function of type void

---
+++ t8_functions/f_void.cpp
@@ -0,0 +1,15 @@
+// create a function with void type
+
+#include <iostream>
+using namespace std;
+
+void sayMessage()
+{
+  cout << "I am a function!" << endl;
+}
+
+int main ()
+{
+  sayMessage();
+  return 0;
+}
